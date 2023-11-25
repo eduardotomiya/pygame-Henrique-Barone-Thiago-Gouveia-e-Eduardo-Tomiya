b24edf259ee15966fdcd832dd0e4035f4f4aded1
Henrique Barone <henriquegpb@al.insper.edu.br>
2023-11-25 20:13:54 -0300

    Fundo, Soms e Atualizações no Codigo principal

--- jogo.py
+++ jogo.py
@@ -1,6 +1,6 @@
 import pygame # Importar funções da biblioteca Pygame
 import random # Importar funções randomicas
-from blocos import Bloco # Comunicar a pasta responsável por blocos do jogo
+from blocos import Block # Comunicar a pasta responsável por blocos do jogo
 from jogador import player # Comunicar a pasta responsável pelo jogador
 
 pygame.init()
@@ -9,23 +9,23 @@ largura = 1000 # Dimensão de largura da tela
 altura = 1000 # Dimensão de largura da tela
 
 # Configurações do fundo
-fundo = pygame.image.load() # Foto do fundo atualizando
+fundo = pygame.image.load('BH1.tiff') # Foto do fundo atualizando
 fundo = pygame.transform.scale(fundo, (largura,altura)) # Ajustar fundo às dimensões da tela
-fundo_inicio = pygame.image.load() # Foto do fundo da tela inicial
+fundo_inicio = pygame.image.load('BH1.tiff') # Foto do fundo da tela inicial
 fundo_inicio = pygame.transform.scale(fundo_inicio,(largura,altura))
 janela = pygame.display.set_mode((largura,altura)) # Criação da janela utilizada para rodar o jogo
 pygame.display.set_caption("Quebra-gelos") # Nome do jogo na Tela Inicial
 
 #Configurações de som
-tocabloco = pygame.mixer.Sound() # Som de quando bola encosta em blocos
-tocatile = pygame.mixer.Sound() # Som de quando bola encosta no bloco do jogador
-somfundo = pygame.mixer.Sound() # Som de fundo do jogo
+tocabloco = pygame.mixer.Sound('Ice Sound.mp3') # Som de quando bola encosta em blocos
+tocatile = pygame.mixer.Sound('TileHit.mp3') # Som de quando bola encosta no bloco do jogador
+somfundo = pygame.mixer.Sound('BGMusic.mp3') # Som de fundo do jogo
 tocatile.set_volume(.5) # Define som mais baixo para quando bola encosta no bloco do jogador
 
 #Configurações de Tela Inicial
 colorwhite = pygame.Color((255,255,255)) # Criação de variável com cor branca
-tipoletra = pygame.font.Font() # Comunicar fonte e tamanho para título
-tipoletra2 = pygame.font.Font() # Comunicar fonte e tamanho para comandos
+tipoletra = pygame.font.Font('Kaph-Italic.ttf', 60) # Comunicar fonte e tamanho para título
+tipoletra2 = pygame.font.Font('Kaph-Italic.ttf', 10) # Comunicar fonte e tamanho para comandos
 textoinicial = tipoletra.render('Ice Wrecker', False, colorwhite) # Aparecer nome do Jogo
 texto1 = tipoletra2.render('Pressione Espaço para Iniciar', False, colorwhite) # Aparecer comando para iniciar o jogo
 
